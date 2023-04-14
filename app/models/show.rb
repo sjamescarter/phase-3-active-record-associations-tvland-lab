16058881fa2c4a37cddd2158820e623331796194
sjamescarter <stephenjamescarter@gmail.com>
2023-04-14 12:00:11 -0500

    commit tree 7a5cb3bd75f1cf9fc8d4750ab7f1427053102539

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def actors_list
+    actors = []
+    self.characters.each { |char| actors << char.actor.full_name }
+    actors
+  end
 end
